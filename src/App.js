aa2b0780221e8af2d0842846dad7053dce493b19
b4ub <baldous@protonmail.com>
2022-02-12 13:26:55 -0600

    simple navigation react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/">Home </Link>|{" "}
+        <Link to="/first">First</Link> |{" "}
+        <Link to="/second">Home</Link>
+      </nav>
+      
+      <h2>Home content</h2>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import First from './pages/First'
+import Second from './pages/Second'
+import NoPage from './pages/NoPage'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+    <Routes> 
+      <Route path="/" element={<App />} />
+      <Route path='first' element={<First/>}/>
+      <Route path='second' element={<Second/>}/>
+      <Route path='*' element={<NoPage/>}/>
+    </Routes>    
+    </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
